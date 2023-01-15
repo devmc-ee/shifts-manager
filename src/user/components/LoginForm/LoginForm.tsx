ecb91f9638da1fe124a67c4e69590416e40e568c
“Roman <lavrunov@gmail.com>
2023-01-15 14:05:20 +0200

    STAF-43: add tabIndex to Icon Button of password field

--- src/user/components/LoginForm/LoginForm.tsx
+++ src/user/components/LoginForm/LoginForm.tsx
@@ -56,6 +56,7 @@ export const LoginForm = ({ handleFieldValueChange, handleSubmit, isSubmitDisabl
               endAdornment={
                 <InputAdornment position="end">
                   <IconButton
+                    tabIndex={-1}
                     disabled={isLoading}
                     aria-label="toggle password visibility"
                     onClick={handleClickShowPassword}
