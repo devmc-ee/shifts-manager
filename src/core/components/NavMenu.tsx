45628773d181c1a9cb6aa1d0aadd9248dbdb5d3e
“Roman <lavrunov@gmail.com>
2023-01-24 23:56:29 +0200

    STAF-33: Make a mobile page layout for protected pages + navbar mockup - to be fixed in the future

--- src/core/components/NavMenu.tsx
+++ src/core/components/NavMenu.tsx
@@ -2,6 +2,13 @@ import { Link, useLocation } from 'react-router-dom';
 import { useAppDispatch } from '../../config/redux/hooks';
 import { clearUserData } from '../../user/redux/userSlice';
 import { routes } from '../routing/routes';
+import { CustomButton } from '../components/buttons/Button/Button';
+import HomeIcon from '@mui/icons-material/Home';
+import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
+import LogoutIcon from '@mui/icons-material/Logout';
+import { useState, MouseEvent } from 'react';
+import { PendingActions } from '@mui/icons-material';
+import './NavMenu.css';
 
 export const NavMenu = (): JSX.Element => {
   const { pathname } = useLocation();
@@ -21,21 +28,56 @@ export const NavMenu = (): JSX.Element => {
 
   const logout = () => dispatch(clearUserData());
 
+  const [activeButton, setActiveButton] = useState('1');
+
+  const handleButtonClick = (event: MouseEvent<HTMLButtonElement>) => {
+    if (event.currentTarget.nodeName !== 'BUTTON') return;
+
+    setActiveButton(event.currentTarget.id);
+  };
+
   return (
-    <nav>
-      <ul>
+    <nav className="nav-menu">
+      <ul className="nav-list">
         <li className={getActiveRouteClass(routes.protected.dashboard)}>
-          <Link to={routes.protected.dashboard}>Dashboard</Link>
+          <Link to={routes.protected.dashboard}>
+            <CustomButton id="1" handleClick={handleButtonClick} className="nav-button" icon={<HomeIcon />} label="Home" active={activeButton === '1'} />
+          </Link>
         </li>
         <li className={getActiveRouteClass(routes.protected.staff)}>
-          <Link to={routes.protected.staff}>Staff</Link>
+          <Link to={routes.protected.staff}>
+            <CustomButton
+              id="2"
+              handleClick={handleButtonClick}
+              className="nav-button"
+              icon={<CalendarMonthIcon />}
+              label="Staff"
+              active={activeButton === '2'}
+            ></CustomButton>
+          </Link>
         </li>
         <li className={getActiveRouteClass(routes.protected.shifts)}>
-          <Link to={routes.protected.shifts}>Shifts</Link>
+          <Link to={routes.protected.shifts}>
+            <CustomButton
+              id="3"
+              handleClick={handleButtonClick}
+              className="nav-button"
+              icon={<PendingActions />}
+              label="Timeshifts"
+              active={activeButton === '3'}
+            ></CustomButton>
+          </Link>
         </li>
         <li>
           <Link onClick={logout} to={routes.login}>
-            Logout
+            <CustomButton
+              id="4"
+              handleClick={handleButtonClick}
+              className="nav-button"
+              icon={<LogoutIcon />}
+              label="Logout"
+              active={activeButton === '4'}
+            ></CustomButton>
           </Link>
         </li>
       </ul>
--- src/core/components/RootNavLayout.tsx
+++ src/core/components/RootNavLayout.tsx
@@ -1,11 +1,15 @@
 import { Outlet } from 'react-router-dom';
 import { NavMenu } from './NavMenu';
+import './RootNavLayout.css';
 
 export const RootNavLayout = (): JSX.Element => {
   return (
-    <>
+    <div className="root-layout-container">
+      <div className="root-layout-header"></div>
+      <div className="root-content-container">
+        <Outlet />
+      </div>
       <NavMenu />
-      <Outlet />
-    </>
+    </div>
   );
 };
