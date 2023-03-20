f7d56db6ea0e64fb7b3faad079a9325dea838cb4
“Roman <lavrunov@gmail.com>
2023-03-20 23:02:02 +0200

    STAF-51: add contacts page, add logout option to profile menu, add user data to profile menu

--- src/user/components/ProfileMenuBlock/ProfileMenuBlock.tsx
+++ src/user/components/ProfileMenuBlock/ProfileMenuBlock.tsx
@@ -1,25 +1,25 @@
-import { useNavigate } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
 import ProfileIconLarge from '../../../assets/icons/profile-icon-large.svg';
 import { PROFILE_MENU_BLOCK, SLIDE_IN_MENU_BLOCK_COMPONENT_NAME } from '../../../assets/menu/slideInMenuData';
 import { useAppDispatch, useAppSelector } from '../../../config/redux/hooks';
 import { AppState } from '../../../config/redux/store';
-import { PublicRoutesEnum } from '../../../config/routes/routes';
 import { MenuItemList } from '../../../core/components/Menus/MenuItemList';
+import { closeSlideInBar } from '../../../core/redux/coreSlice';
 import { clearUserData } from '../../redux/userSlice';
 
 export const ProfileMenuBlock = (): JSX.Element => {
+  const dispatch = useAppDispatch();
+  const { t } = useTranslation();
+
   const { userName, userEmail } = useAppSelector(({ user }: AppState) => ({
     userName: user.userFullName,
     userEmail: user.userName,
   }));
 
-  const navigate = useNavigate();
-  const dispatch = useAppDispatch();
-
-  const handleLogOut = (): void => {
+  const handleLogOut = () => {
     localStorage.clear();
-    dispatch(clearUserData);
-    navigate(PublicRoutesEnum.login);
+    dispatch(closeSlideInBar());
+    setTimeout(() => dispatch(clearUserData()), 230);
   };
 
   return (
@@ -33,8 +33,8 @@ export const ProfileMenuBlock = (): JSX.Element => {
         </div>
       </div>
       <MenuItemList itemList={PROFILE_MENU_BLOCK} componentName={SLIDE_IN_MENU_BLOCK_COMPONENT_NAME.RIGHT_MENU} />
-      <div role="button" tabIndex={0} className="menu-list-item ripple" onClick={handleLogOut} onKeyDown={handleLogOut}>
-        <span className="menu-list-item-title">Log out</span>
+      <div role="button" tabIndex={-1} className="menu-list-item ripple" onClick={handleLogOut} onKeyDown={handleLogOut}>
+        <span className="menu-list-item-title">{t(`core.slideInBar.profileMenu.logout`)}</span>
       </div>
     </div>
   );
