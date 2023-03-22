218c9c5dc4c2595492b5521c9e0c525a00963a29
“Roman <lavrunov@gmail.com>
2023-03-22 20:34:56 +0200

    STAF-51: fix SlideInBarMenuHeader stories

--- src/core/components/SlideInBarMenuHeader/SlideInBarMenuHeader.stories.tsx
+++ src/core/components/SlideInBarMenuHeader/SlideInBarMenuHeader.stories.tsx
@@ -1,7 +1,7 @@
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 import { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
-import { SLIDE_FROM_SIDE, SLIDE_IN_MENU_BLOCK_COMPONENT_NAME } from '../../../assets/menu/slideInMenuData';
+import { SLIDE_IN_MENU_BLOCK_COMPONENT_NAME } from '../../../assets/menu/slideInMenuData';
 import { SlideInBarMenuHeader, SlideInBarMenuHeaderProps } from './SlideInBarMenuHeader';
 
 export default {
@@ -9,27 +9,20 @@ export default {
   component: SlideInBarMenuHeader,
 } as ComponentMeta<typeof SlideInBarMenuHeader>;
 
-const Template: ComponentStory<typeof SlideInBarMenuHeader> = ({ from }: SlideInBarMenuHeaderProps) => {
+const Template: ComponentStory<typeof SlideInBarMenuHeader> = ({ componentName }: SlideInBarMenuHeaderProps) => {
   const { t } = useTranslation();
   const [headerTitle, setHeaderTitle] = useState('');
-  const [componentName, setComponentName] = useState(SLIDE_IN_MENU_BLOCK_COMPONENT_NAME.RIGHT_MENU);
-
-  useEffect(() => {
-    if (from === SLIDE_FROM_SIDE.LEFT) {
-      setComponentName(SLIDE_IN_MENU_BLOCK_COMPONENT_NAME.LEFT_MENU);
-    }
-  }, [from]);
 
   useEffect(() => {
     const newTitle = t(`core.slideInBar.${componentName}.title`);
     setHeaderTitle(newTitle);
   }, [componentName, t]);
 
-  return <SlideInBarMenuHeader from={from} title={headerTitle} />;
+  return <SlideInBarMenuHeader componentName={componentName} title={headerTitle} />;
 };
 
 export const MainMenuHeader = Template.bind({});
-MainMenuHeader.args = { from: SLIDE_FROM_SIDE.LEFT };
+MainMenuHeader.args = { componentName: SLIDE_IN_MENU_BLOCK_COMPONENT_NAME.LEFT_MENU };
 
 export const ProfileMenuHeader = Template.bind({});
-ProfileMenuHeader.args = { from: SLIDE_FROM_SIDE.RIGHT };
+ProfileMenuHeader.args = { componentName: SLIDE_IN_MENU_BLOCK_COMPONENT_NAME.RIGHT_MENU };
